63c1ec1f00186d923aff2b6f709feb3caa9b4678
jyukito18 <jyukito.18@gmail.com>
2024-10-03 21:55:38 +0900

    bmiを変数に追加しました

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,7 @@
-let bodyWeight, bodyHeight;
+let bodyWeight, bodyHeight, bmi;
 
 bodyWeight = 68;
 bodyHeight = 1.7;
+bmi = bodyWeight / (bodyHeight * bodyHeight);
 
-console.log(bodyWeight/(bodyHeight*bodyHeight));
+console.log(bmi);
